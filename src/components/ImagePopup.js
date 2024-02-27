87a7d7f1a221646f27ddebdb6402a9126a0575ac
Maricarmen <nemracira1718@gmail.com>
2024-02-27 13:48:55 -0500

    correccion modal

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,9 +3,9 @@ import close from '../images/__CloseIcon.png'
 
 function ImagePopup(props) {
   return (
-    <div className="modal" id="modalPopup">
+    <div >
     <div className={`modal__popup-img ${props.isOpen ? 'popup__opened':''}`} >
-            <img src={props.card.link} alt="imagen modal" className="modal__popup-img-background" id="cardPopup"/>
+            <img src={props.card.link} alt={props.card.name} className="modal__popup-img-background" id="cardPopup"/>
             <img src={close} alt="Equis para cerrar el formulario" className="modal__popup-img-close" id="closeImgBtn" onClick={props.onClose}/>
             <p className="modal__popup-img-title" id="popupImgTitle">{props.card.name}</p>
     </div>
